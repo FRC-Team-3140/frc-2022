cb59247267ba94f72e169ca27c304a3d520d74d4
omgeometry <77588336+omgeometry@users.noreply.github.com>
2022-03-08 17:24:41 -0500

    Update TurnToAngle.java

--- src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
+++ src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
@@ -1,49 +1,37 @@
 package frc.robot.commands.drivetrain;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase
+import frc.robot.HardwareAdapter;
 import frc.robot.RobotContainer;
 
 public class TurnToAngle extends CommandBase {
-    private Timer timer = new Timer();
     private double throttle = 0;
-    private double time = 0;
+    private double angle = 0;
 
-    // public TurnToAngle(double throttle, double time) {
-    //   this.throttle = throttle;    (-throttle, throttle  or  throttle, -throttle)
-    //   this.angle = angle;
-    //   this.time = time;
-    //   addRequirements(RobotContainer.dt);
-  // }
-
-    public TurnToAngle(double throttle, double time) {
+    public TurnToAngle(double throttle, double angle) {
     this.throttle = throttle;
-    this.time = time;
+    this.angle = angle;
     addRequirements(RobotContainer.dt);
   }
 
   @Override
   public void initialize() {
-    // angle.get();
-    timer.start();
   }
 
   @Override
   public void execute() {
-    // RobotContainer.dt.turnToAngle(if __ turn ___ else turn ___)
     RobotContainer.dt.tankDrive(-throttle, throttle);
-  }
+  } 
 
+
+  
   @Override
   public void end(boolean interrupted) {
       RobotContainer.dt.tankDrive(0, 0);
-      timer.stop();
-      timer.reset();
   }
 
   @Override
   public boolean isFinished() {
-    // return angle.get() == angle;
-    return timer.get() > time;
+    return (HardwareAdapter.navx.getAngle()+ 360) % 360 <= angle;
   }
-}
+} 
