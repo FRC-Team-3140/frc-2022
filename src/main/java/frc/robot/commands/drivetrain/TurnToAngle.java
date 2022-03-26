afc33922e28a3172c67c962f6ec6c4da8ff0837a
Channing Tan <channingtan@gmail.com>
2022-03-26 10:18:45 -0400

    update turn to angle

--- src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
+++ src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
@@ -1,21 +1,23 @@
 package frc.robot.commands.drivetrain;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.HardwareAdapter;
 import frc.robot.RobotContainer;
-
-public class TurnToAngle extends CommandBase {
+public class TurnToAngle extends CommandBase{
     private double throttle = 0;
     private double angle = 0;
+    private double tolerance = 0;
 
-    public TurnToAngle(double throttle, double angle) {
+
+    public TurnToAngle(double throttle, double angle, double tolerance) {
     this.throttle = throttle;
     this.angle = angle;
+    this.tolerance = tolerance;
     addRequirements(RobotContainer.dt);
   }
 
   @Override
   public void initialize() {
+    HardwareAdapter.navx.reset();
   }
 
   @Override
@@ -32,6 +34,7 @@ public class TurnToAngle extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return (HardwareAdapter.navx.getAngle()+ 360) % 360 <= angle;
+    // return HardwareAdapter.navx.getYaw() + 180   <= angle + 5  && HardwareAdapter.navx.getYaw() + 180 >= angle - 5; 
+    return Math.abs(angle - HardwareAdapter.navx.getYaw()) <= this.tolerance;
   }
 } 
